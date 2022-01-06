39092146e95ceb4398936e03a0e845d917134cc6
Tom Elliott <ipse@paregorios.org>
2022-01-06 16:31:51 -0600

    populate setup.py with basic starting info, including switch to GNU Affero GPL

--- setup.py
+++ setup.py
@@ -4,21 +4,19 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="{distribution-name}",
+    name="pleiades_amphitheaters",
     version="0.0.1",
-    author="{author}",
-    author_email="{author-email}",
-    description="{short-description}",
-    license='MIT',
+    author="Tom Elliott",
+    author_email="tom.elliott@nyu.edu",
+    description="Code for collating Sebastian Heath's Roman Amphitheaters dataset with the Pleiades gazetteer of ancient places.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    {project-url}
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: {pyver}",
-        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10.1",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
         "Operating System :: OS Independent",
     ],
     install_requires=['airtight'],
-    python_requires='>={pyver}'
+    python_requires='>=3.10.1'
 )
