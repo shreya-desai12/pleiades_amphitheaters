2a7ad058286675ae97ac86abb433c5d6b72e5947
Tom Elliott <ipse@paregorios.org>
2022-01-11 16:18:16 -0600

    added docstrings at class and method levels for amphitheater_data module in accordance with python PEP257 Docstring conventions

--- pleiades_amphitheaters/amphitheaters_data.py
+++ pleiades_amphitheaters/amphitheaters_data.py
@@ -8,7 +8,17 @@ import requests
 import json
 
 class ParseData:
+    """
+    Fetch sfheath amphitheater data over the web and return parsed JSON.
+    """
+
     def read_data(self):
+        """
+        Perform fetch and parse.
+        
+        :returns: the fetched JSON data
+        :rtype: dict or list
+        """
         response = requests.get('https://raw.githubusercontent.com/roman-amphitheaters/roman-amphitheaters/main/roman-amphitheaters.geojson')
         if response.status_code == 200:
             # print(response.status_code)
