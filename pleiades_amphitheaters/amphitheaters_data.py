187042a064cdf901d358abc4640ecfaad1726e3b
shreya-desai12 <shreyadesai1202@yahoo.com>
2022-01-11 08:00:07 -0500

    Fetching sfheath's roman amphitheater's data

---
+++ pleiades_amphitheaters/amphitheaters_data.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Fetch data from sfheath's Github repository URI
+"""
+
+import requests
+import json
+
+class Parse_data:
+    def read_data(self):
+        response = requests.get('https://raw.githubusercontent.com/roman-amphitheaters/roman-amphitheaters/main/roman-amphitheaters.geojson')
+        if response.status_code == 200:
+            # print(response.status_code)
+            json_data = json.loads(response.text)
+            # print(type(json_data))   
+            # print(json_data)             
+            return json_data
+        else:
+            print("Error in retrieving the data")
+            return {}
+        
+rd = Parse_data()
+final_data = rd.read_data()
+
