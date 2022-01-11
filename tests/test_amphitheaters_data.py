94e2572213e77d87dd226c9d91232a71b2e2e701
Tom Elliott <ipse@paregorios.org>
2022-01-11 17:19:05 -0600

    fix test classname

--- tests/test_amphitheaters_data.py
+++ tests/test_amphitheaters_data.py
@@ -10,7 +10,7 @@ from unittest import TestCase
 
 logger = logging.getLogger(__name__)
 
-class Test_ParseData(TestCase):
+class Test_AmphitheaterData(TestCase):
 
     def test_parse(self):
         parser = AmphitheaterData()
