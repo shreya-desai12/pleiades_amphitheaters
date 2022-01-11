8a14d098143a282ade009973e2f37207b060541c
Tom Elliott <ipse@paregorios.org>
2022-01-11 16:56:28 -0600

    add test for pleiades_data module

---
+++ tests/test_pleiades_data.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# -*- coding: utf-8 -*-
+"""Nose tests for pleiades_data module."""
+
+import logging
+from pprint import pprint
+from pleiades_amphitheaters.pleiades_data import PleiadesData
+from unittest import TestCase
+
+logger = logging.getLogger(__name__)
+
+class Test_PleiadesData(TestCase):
+
+    def test_parse(self):
+        parser = PleiadesData()
+        d = parser.read_data(uri='https://pleiades.stoa.org/places/39414/json')
+        assert isinstance(d, dict) == True
+        print(d['@type'])
+        assert d['@type'] == 'Place'
+
